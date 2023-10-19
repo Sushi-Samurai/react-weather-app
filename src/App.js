9cd3c4e2e1af202e772f3d3831a2be6c6ebbedff
Sushi-Samurai <122423970+Sushi-Samurai@users.noreply.github.com>
2023-10-18 21:42:01 -0700

    Added boostrap to project

--- src/App.js
+++ src/App.js
@@ -3,30 +3,32 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather app</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.shecodes.io/graduates/63509-sam-turner-lara"
-          target="_blank"
-        >
-          Sam Turner
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/Sushi-Samurai/react-weather-app"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>{" "}
-        and{" "}
-        <a
-          href="https://react-weather-app-sushisam.netlify.app/"
-          target="_blank"
-        >
-          hosted on Netlify
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather app</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.shecodes.io/graduates/63509-sam-turner-lara"
+            target="_blank"
+          >
+            Sam Turner
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Sushi-Samurai/react-weather-app"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://react-weather-app-sushisam.netlify.app/"
+            target="_blank"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
