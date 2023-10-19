f3747f7feb0601b4401612c64ca5f7cb11a2030f
Sushi-Samurai <122423970+Sushi-Samurai@users.noreply.github.com>
2023-10-18 21:57:11 -0700

    Built weather app format

---
+++ src/Weather.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city"
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+      <h1>Sacramento</h1>
+      <ul>
+        <li>Wednesday 21:45</li>
+        <li>Clear Sky</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-night.png"
+            alt="Clear Sky"
+          />
+          68°F
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation 15%</li>
+            <li>Humidity: 64%</li>
+            <li>Wind: 5 mph</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
