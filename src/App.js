5b634b5be371d1ed4149d53f0f68cec5f3217c19
Sushi-Samurai <122423970+Sushi-Samurai@users.noreply.github.com>
2023-10-18 20:04:34 -0700

    Added GitHub Repo URL to footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather app</div>;
+  return (
+    <div className="App">
+      <h1>Weather app</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://www.shecodes.io/graduates/63509-sam-turner-lara"
+          target="_blank"
+        >
+          Sam Turner
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/Sushi-Samurai/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://react-weather-app-sushisam.netlify.app/"
+          target="_blank"
+        >
+          hosted on Netlify
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
